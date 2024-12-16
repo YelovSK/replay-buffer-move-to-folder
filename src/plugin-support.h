f078b64f6d21f0163a814aac0e622f0f98d17e0d
YelovSK <hampatrik@gmail.com>
2024-12-16 15:40:57 +0100

    Move on save

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+Replay Buffer - move to folder
+Copyright (C) 2024 Yelov
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -26,12 +26,22 @@ extern "C" {
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <windows.h>
+#include <psapi.h>
 
 extern const char *PLUGIN_NAME;
 extern const char *PLUGIN_VERSION;
 
 void obs_log(int log_level, const char *format, ...);
 extern void blogva(int log_level, const char *format, va_list args);
+
+const int get_last_replay_path(char *buffer);
+int get_active_window_path(char *buffer, int buffer_size);
+BOOL is_ignored_path(const char *path);
+int get_executable_path(HWND hwnd, char *buffer, int buffer_size);
+BOOL move_file_to_new_location(const char *source_file_path, const char *exe_file_path);
+void get_path_without_file(const char *full_path, char *path_without_file);
+void convert_tchar_to_char(const TCHAR *source, char *dest, size_t dest_size);
 
 #ifdef __cplusplus
 }
